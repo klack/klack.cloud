b50716029f449949d5ba1e736e937753b9d91303
klack <rlayton@gmail.com>
2024-08-31 19:24:01 -0600

    Template for traefik host ip setting

---
+++ config/traefik/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Read environment variables
+HOST_IP=${HOST_IP}
+
+# Substitute environment variables into dynamic_conf.yml using a temporary file
+sed "s|\${HOST_IP}|${HOST_IP}|g" \
+     /config/dynamic_conf.yml.template > /config/dynamic_conf.yml
+
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- traefik "$@"
+fi
+
+# if our command is a valid Traefik subcommand, let's invoke it through Traefik instead
+# (this allows for "docker run traefik version", etc)
+if traefik "$1" --help >/dev/null 2>&1
+then
+    set -- traefik "$@"
+else
+    echo "= '$1' is not a Traefik command: assuming shell execution." 1>&2
+fi
+
+exec "$@"
