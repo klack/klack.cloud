f9493372049cd01ee85cf7b76f60b9b6d9712885
klack <rlayton@gmail.com>
2024-09-17 23:58:49 -0600

    Refactor

--- scripts/gen_config.sh
+++ scripts/gen_config.sh
@@ -69,7 +69,9 @@ else
     echo "Hosts file already modified."
 fi
 
-#Prompt for info
+#.env file generation
+TIMEZONE=$(timedatectl | grep "Time zone" | awk '{print $3}')
+#Password setup
 read -p "Enter external domain: " EXTERNAL_DOMAIN
 read -s -p "Create an admin password: " PASSWORD
 echo
@@ -87,42 +89,15 @@ if [ "$CLOUD_PASSWORD" != "$CLOUD_PASSWORD_CONFIRM" ]; then
     echo "Passwords do not match. Exiting."
     exit 1
 fi
-read -p "Visit https://plex.tv/claim and paste claim token: " PLEX_CLAIM
-read -p "Press enter for default network interface [$DEFAULT_INTERFACE]: " HOST_IP
-read -p "Press enter for default host IP [$DEFAULT_HOST_IP]: " HOST_IP
-read -p "Press enter for default gateway [$DEFUALT_GATEWAY]: " GATEWAY
-DEFAULT_HOST_IP=$(hostname -I | awk '{print $1}')
-DEFUALT_GATEWAY=$(ip route | grep default | awk '{print $3}')
-DEFAULT_INTERFACE=$(ip route | grep default | awk '{print $5}')
-TIMEZONE=$(timedatectl | grep "Time zone" | awk '{print $3}')
-HOST_IP=${HOST_IP:-"$DEFAULT_HOST_IP"}
-GATEWAY=${GATEWAY:-"$DEFUALT_GATEWAY"}
-NETWORK=$(echo "$GATEWAY" | cut -d '.' -f 1-3)
 ESCAPED_PASSWORD=$(printf '%s\n' "$PASSWORD" | sed 's/\([\"$]\)/\\\1/g')
+ESCAPED_CLOUD_PASSWORD=$(printf '%s\n' "$CLOUD_PASSWORD" | sed 's/\([\"$]\)/\\\1/g')
 MARIADB_PASSWORD=$(< /dev/urandom tr -dc 'A-Za-z0-9!@#%' | head -c 16)
 MARIADB_ROOT_PASSWORD=$(< /dev/urandom tr -dc 'A-Za-z0-9!@#%' | head -c 16)
 SONARR_API_KEY=$(head -c 16 /dev/urandom | xxd -p)
 RADARR_API_KEY=$(head -c 16 /dev/urandom | xxd -p)
 
-#Update .env file
-cp -p ./.env.template ./.env
-sed -i "s|^TZ=.*|TZ=$TIMEZONE|" .env
-sed -i "s|^EXTERNAL_DOMAIN=.*|EXTERNAL_DOMAIN=$EXTERNAL_DOMAIN|" .env
-sed -i "s|^PLEX_CLAIM=.*|PLEX_CLAIM=$PLEX_CLAIM|" .env
-sed -i "s|^BASIC_AUTH_PASS=.*|BASIC_AUTH_PASS=\"$ESCAPED_PASSWORD\"|" .env
-sed -i "s|^NODE_EXPORTER_PASS=.*|NODE_EXPORTER_PASS=\"$ESCAPED_PASSWORD\"|" .env
-sed -i "s|^PHOTOPRISM_ADMIN_PASSWORD=.*|PHOTOPRISM_ADMIN_PASSWORD=\"$ESCAPED_PASSWORD\"|" .env
-sed -i "s|^MARIADB_ROOT_PASSWORD=.*|MARIADB_ROOT_PASSWORD=\"$MARIADB_ROOT_PASSWORD\"|" .env
-sed -i "s|^MARIADB_PASSWORD=.*|MARIADB_PASSWORD=\"$MARIADB_PASSWORD\"|" .env
-sed -i "s|^HOST_IP=.*|HOST_IP=$HOST_IP|" .env
-sed -i "s|^HONEYPOT_GATEWAY=.*|HONEYPOT_GATEWAY=$GATEWAY|" .env
-sed -i "s|^NETWORK_INTERFACE=.*|NETWORK_INTERFACE=$DEFAULT_INTERFACE|" .env
-sed -i "s|^HOST_IP=.*|HOST_IP=$HOST_IP|" .env
-sed -i "s|^SONARR_API_KEY=.*|SONARR_API_KEY=\"$SONARR_API_KEY\"|" .env
-sed -i "s|^RADARR_API_KEY=.*|RADARR_API_KEY=\"$RADARR_API_KEY\"|" .env
-sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" .env
-sed -i "s/\${NETWORK}/${NETWORK}/g" .env
-echo ".env file generated"
+#Plex
+read -p "Visit https://plex.tv/claim and paste claim token: " PLEX_CLAIM
 
 #Set qBittorrent password
 cp -p ./config/qbittorrent/qBittorrent.conf.template ./config/qbittorrent/qBittorrent.conf
@@ -139,3 +114,35 @@ sed -i "s/\${API_KEY}/${SONARR_API_KEY}/g" ./config/sonarr/config.xml
 docker run --rm httpd:latest htpasswd \
   -Bbn admin "$PASSWORD" > \
   ./config/traefik/htpasswd && echo "htpassword generated"
+
+#Honeypot
+DEFAULT_INTERFACE=$(ip route | grep default | awk '{print $5}')
+DEFAULT_HOST_IP=$(hostname -I | awk '{print $1}')
+DEFUALT_GATEWAY=$(ip route | grep default | awk '{print $3}')
+read -p "Press enter for default network interface [$DEFAULT_INTERFACE]: " HOST_IP
+read -p "Press enter for default host IP [$DEFAULT_HOST_IP]: " HOST_IP
+read -p "Press enter for default gateway [$DEFUALT_GATEWAY]: " GATEWAY
+HOST_IP=${HOST_IP:-"$DEFAULT_HOST_IP"}
+GATEWAY=${GATEWAY:-"$DEFUALT_GATEWAY"}
+NETWORK=$(echo "$GATEWAY" | cut -d '.' -f 1-3)
+
+#Update .env file
+cp -p ./.env.template ./.env
+sed -i "s|^TZ=.*|TZ=$TIMEZONE|" .env
+sed -i "s|^EXTERNAL_DOMAIN=.*|EXTERNAL_DOMAIN=$EXTERNAL_DOMAIN|" .env
+sed -i "s|^PLEX_CLAIM=.*|PLEX_CLAIM=$PLEX_CLAIM|" .env
+sed -i "s|^BASIC_AUTH_PASS=.*|BASIC_AUTH_PASS=\"$ESCAPED_PASSWORD\"|" .env
+sed -i "s|^NODE_EXPORTER_PASS=.*|NODE_EXPORTER_PASS=\"$ESCAPED_PASSWORD\"|" .env
+sed -i "s|^PHOTOPRISM_ADMIN_PASSWORD=.*|PHOTOPRISM_ADMIN_PASSWORD=\"$ESCAPED_PASSWORD\"|" .env
+sed -i "s|^CLOUD_PASS=.*|CLOUD_PASS=\"$ESCAPED_CLOUD_PASSWORD\"|" .env
+sed -i "s|^MARIADB_ROOT_PASSWORD=.*|MARIADB_ROOT_PASSWORD=\"$MARIADB_ROOT_PASSWORD\"|" .env
+sed -i "s|^MARIADB_PASSWORD=.*|MARIADB_PASSWORD=\"$MARIADB_PASSWORD\"|" .env
+sed -i "s|^HOST_IP=.*|HOST_IP=$HOST_IP|" .env
+sed -i "s|^HONEYPOT_GATEWAY=.*|HONEYPOT_GATEWAY=$GATEWAY|" .env
+sed -i "s|^NETWORK_INTERFACE=.*|NETWORK_INTERFACE=$DEFAULT_INTERFACE|" .env
+sed -i "s|^HOST_IP=.*|HOST_IP=$HOST_IP|" .env
+sed -i "s|^SONARR_API_KEY=.*|SONARR_API_KEY=\"$SONARR_API_KEY\"|" .env
+sed -i "s|^RADARR_API_KEY=.*|RADARR_API_KEY=\"$RADARR_API_KEY\"|" .env
+sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" .env
+sed -i "s/\${NETWORK}/${NETWORK}/g" .env
+echo ".env file generated"
