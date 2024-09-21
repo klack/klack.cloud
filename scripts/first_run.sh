a4c41a3d317d8d3e9b8e2686282c6476a93b3447
klack <rlayton@gmail.com>
2024-09-21 17:50:47 -0600

    Refactor folders

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -41,6 +41,9 @@ nohup /usr/local/bin/node_exporter >/dev/null 2>&1 &
 #Run first time app scripts
 ./config/sftpgo/provision.sh
 
+#Download Sample Files
+./scripts/download_samples.sh
+
 #Generate home page
 cp ./index.html.template index.html
 chown 1000:1000 ./index.html
@@ -48,12 +51,3 @@ sed -i "s/\${INTERNAL_DOMAIN}/${INTERNAL_DOMAIN}/g" index.html
 sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" index.html
 
 echo -e "\nIndex.html created"
-
-#Sample files
-# echo  -e "\nDownloading sample files"
-# wget https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_30fps_normal.mp4.zip
-# unzip bbb_sunflower_1080p_30fps_normal.mp4.zip -d ./data/klack.tv/library/movies
-# rm bbb_sunflower_1080p_30fps_normal.mp4.zip
-# wget -O ./data/sftpgoroot/data/cloud/Photos/starry_night.jpg https://upload.wikimedia.org/wikipedia/commons/c/cd/VanGogh-starry_night.jpg
-# wget -O ./data/sftpgoroot/data/cloud/Photos/over_the_rhone.jpg https://upload.wikimedia.org/wikipedia/commons/9/94/Starry_Night_Over_the_Rhone.jpg
-# wget -O ./data/sftpgoroot/data/cloud/Photos/vincent_van_gogh.jpg "https://upload.wikimedia.org/wikipedia/commons/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg"
