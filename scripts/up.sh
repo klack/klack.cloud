a460328d9ae4cb867bfb14f72bf0fac3f84b7557
klack <rlayton@gmail.com>
2024-09-22 15:58:35 -0600

    up

--- scripts/up.sh
+++ scripts/up.sh
@@ -3,4 +3,12 @@
 if [ "$(basename "$(dirname "$PWD")")" = "scripts" ]; then
   cd ..
 fi
-docker compose --profile apps --profile downloaders up -d
+
+# Check if ./config/wg0.conf exists
+if [ -f "./config/wireguard/wg0.conf" ]; then
+  # If wg0.conf exists, run docker compose with profile apps and downloaders
+  docker compose --profile apps --profile downloaders up -d
+else
+  # If wg0.conf does not exist, run docker compose without the profiles
+  docker compose --profile apps up -d
+fi
