d1cf0e32fbcfc22357e33ba3f40068e115b126d3
klack <rlayton@gmail.com>
2024-10-08 00:47:33 -0600

    Reorder plex init

--- start.sh
+++ start.sh
@@ -20,11 +20,12 @@ fi
 echo -e "\nStarting"
 docker compose --profile apps up -d
 
-#Generate home page
-cp ./web/index.html.template ./web/index.html
-sed -i "s/\${INTERNAL_DOMAIN}/${INTERNAL_DOMAIN}/g" ./web/index.html
-sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" ./web/index.html
-sed -i "s/\${HOST_IP}/${HOST_IP}/g" ./web/index.html
+# Check if plex claim token was set
+if [ -n "$PLEX_CLAIM" ]; then
+  docker compose up plex -d
+else
+  sed -i '/#video {/{N;s/display: block;/display: none;/}' ./web/index.html # Disable panel on homepage
+fi
 
 # Check if ./vpn.conf exists to run download managers
 if [ -f "./vpn.conf" ]; then
@@ -33,13 +34,11 @@ else
   sed -i '/#download_managers {/{N;s/display: block;/display: none;/}' ./web/index.html # Disable panel on homepage
 fi
 
-# Check if plex claim token was set
-if [ -n "$PLEX_CLAIM" ]; then
-  docker compose up plex -d
-else
-  sed -i '/#video {/{N;s/display: block;/display: none;/}' ./web/index.html # Disable panel on homepage
-fi
-
+#Generate home page
+cp ./web/index.html.template ./web/index.html
+sed -i "s/\${INTERNAL_DOMAIN}/${INTERNAL_DOMAIN}/g" ./web/index.html
+sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" ./web/index.html
+sed -i "s/\${HOST_IP}/${HOST_IP}/g" ./web/index.html
 echo -e "\nIndex.html created"
 
 #Show home page
