33a610e3f199acc43c076347f6f85123d33042df
klack <rlayton@gmail.com>
2024-10-08 19:34:29 -0600

    Rename plex server

--- config/plex/provision.sh
+++ config/plex/provision.sh
@@ -15,6 +15,7 @@ else
     tar -xzf ./config/plex/provision.tar.gz -C $DIR_DATA_ROOT
     cp ./config/plex/Preferences.xml.template "$DIR_DATA_ROOT/plex/Library/Application Support/Plex Media Server/Preferences.xml"
     sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" "$DIR_DATA_ROOT/plex/Library/Application Support/Plex Media Server/Preferences.xml"
+    sed -i "s/\${CLOUD_USER}/${CLOUD_USER}/g" "$DIR_DATA_ROOT/plex/Library/Application Support/Plex Media Server/Preferences.xml"
 
     #Download sample movie
     source ./.env
