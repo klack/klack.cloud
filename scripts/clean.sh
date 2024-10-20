4adfa6843258ec77a2fe06e698d3910f710f822c
klack <rlayton@gmail.com>
2024-10-19 19:27:53 -0600

    Don't delete ssl certs

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -22,7 +22,7 @@ chown -R 1000:1000 *
 
 #Remove docker volumes
 echo "Deleting docker volumes"
-docker volume ls -q | grep '^klack-cloud_' | xargs -r docker volume rm -f
+docker volume ls -q | grep '^klack-cloud_' | grep -v '^klack-cloud_acme$' | xargs -r docker volume rm -f
 
 #Remove log, cloud, and data directories
 echo "Removing Data and Log Directories"
