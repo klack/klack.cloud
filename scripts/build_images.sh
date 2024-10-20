c68a598c14966714edb830d0d286f97994f28b05
klack <rlayton@gmail.com>
2024-10-19 22:10:48 -0600

    .

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -4,8 +4,8 @@ source ./.env
 
 #Build docker images
 if [ $PLATFORM == "linux/arm64" ]; then
-  echo -e "\nBuilding Plex"
-  docker build --platform linux/arm64 --pull --no-cache -t plexinc/pms-docker ./config/plex/git
-  echo -e "\nBuilding Dionaea"
-  docker build --platform linux/arm64 --pull --no-cache -t dinotools/dionaea ./config/dionaea/git
+    echo -e "\nBuilding Plex"
+    docker build --platform linux/arm64 --pull --no-cache -t plexinc/pms-docker ./config/plex/git
+    echo -e "\nBuilding Dionaea"
+    docker build --platform linux/arm64 --pull --no-cache -t dinotools/dionaea ./config/dionaea/git
 fi
