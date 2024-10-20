07bbdc89738c0f75f311a3d1516cb26c73a40076
klack <rlayton@gmail.com>
2024-10-19 20:20:26 -0600

    Download sample first

--- config/plex/provision.sh
+++ config/plex/provision.sh
@@ -3,6 +3,16 @@
 source ./.env
 
 echo -e "\nPlex Setup"
+
+#Download sample movie
+source ./.env
+echo -e "\nDownloading sample files"
+wget -q --show-progress https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_30fps_normal.mp4.zip
+unzip bbb_sunflower_1080p_30fps_normal.mp4.zip -d $DIR_CLOUD_ROOT/$CLOUD_USER/Library/Movies
+rm bbb_sunflower_1080p_30fps_normal.mp4.zip
+mv $DIR_CLOUD_ROOT/$CLOUD_USER/Library/Movies/bbb_sunflower_1080p_30fps_normal.mp4 "$DIR_CLOUD_ROOT/$CLOUD_USER/Library/Movies/Big Buck Bunny (2008).mp4"
+
+#Ask for claim token
 read -p "Visit https://plex.tv/claim and paste claim token, or press enter to skip: " PLEX_CLAIM
 
 if [ -z "$PLEX_CLAIM" ]; then
@@ -16,14 +26,6 @@ else
     cp ./config/plex/Preferences.xml.template "$DIR_DATA_ROOT/plex/Library/Application Support/Plex Media Server/Preferences.xml"
     sed -i "s/\${EXTERNAL_DOMAIN}/${EXTERNAL_DOMAIN}/g" "$DIR_DATA_ROOT/plex/Library/Application Support/Plex Media Server/Preferences.xml"
     sed -i "s/\${CLOUD_USER}/${CLOUD_USER}/g" "$DIR_DATA_ROOT/plex/Library/Application Support/Plex Media Server/Preferences.xml"
-
-    #Download sample movie
-    source ./.env
-    echo -e "\nDownloading sample files"
-    wget -q --show-progress https://download.blender.org/demo/movies/BBB/bbb_sunflower_1080p_30fps_normal.mp4.zip
-    unzip bbb_sunflower_1080p_30fps_normal.mp4.zip -d $DIR_CLOUD_ROOT/$CLOUD_USER/Library/Movies
-    rm bbb_sunflower_1080p_30fps_normal.mp4.zip
-    mv $DIR_CLOUD_ROOT/$CLOUD_USER/Library/Movies/bbb_sunflower_1080p_30fps_normal.mp4 "$DIR_CLOUD_ROOT/$CLOUD_USER/Library/Movies/Big Buck Bunny (2008).mp4"
     
     echo -e "\nPlex Setup complete"
 fi
