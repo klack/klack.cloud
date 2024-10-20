6f99d4638cde56dcb979d5fcbcfcc11e873353f9
klack <rlayton@gmail.com>
2024-10-19 23:01:04 -0600

    Order

--- scripts/platform.sh
+++ scripts/platform.sh
@@ -5,9 +5,6 @@ source ./.env
 #Platform Specific
 PWD=$(pwd)
 if [ $PLATFORM == "linux/arm64" ]; then
-    #Build Images
-    ./scripts/build_images.sh
-
     #Manual update script for plex and dionea
     if ! grep -q "build_images.sh" /etc/crontab; then
         sudo sh -c "sudo echo -e \"0 4 * * * $USER $PWD/scripts/build_images.sh && $PWD/start.sh\" >> /etc/crontab"
@@ -15,4 +12,7 @@ if [ $PLATFORM == "linux/arm64" ]; then
     else
         echo "build_images.sh already added to crontab"
     fi
+
+    #Build Images
+    ./scripts/build_images.sh
 fi
