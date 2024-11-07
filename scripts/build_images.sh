8474ccc6879284564b20149dc43c065d7cf1610c
klack <rlayton@gmail.com>
2024-11-07 10:50:33 -0700

    Fix build images

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-source ./.env
+# Get the architecture
+ARCH=$(uname -m)
+
+# Map architectures to platform strings
+case "$ARCH" in
+x86_64)
+  PLATFORM="linux/amd64"
+  ;;
+aarch64)
+  PLATFORM="linux/arm64"
+  ;;
+*)
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+  ;;
+esac
 
 #Build docker images
 if [ $PLATFORM == "linux/arm64" ]; then
