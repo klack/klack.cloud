e713c5a4467e43ffef469d0bb4bfc19c36c144c7
klack <rlayton@gmail.com>
2024-11-13 12:38:45 -0700

    Dont delete data directory

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -36,7 +36,7 @@ LOG_DIRS=(
     "/var/log/cowrie"
     "/var/log/plex/PMS Plugin Logs"
 )
-rm -rfv ./cloud-metadata ./data ./backups ./cloud "${LOG_DIRS[@]}"
+rm -rfv ./cloud-metadata ./backups ./cloud "${LOG_DIRS[@]}"
 
 #Remove node_exporter
 echo "Removing node_exporter"
